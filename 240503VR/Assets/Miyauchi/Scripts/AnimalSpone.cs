1ef6c41d4ab1b7376d6ed99e8bc5640c5f48f065
k21rs125 <98999985+k21rs125@users.noreply.github.com>
2024-05-03 20:13:59 +0900

    スポーン位置

---
+++ 240503VR/Assets/Miyauchi/Scripts/AnimalSpone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpone : MonoBehaviour
+{
+    GameObject[] spones;
+    GameObject[] animals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// スポーン位置を決める
+    /// </summary>
+    private void sponeDeside()
+    {
+        int rnd_spone = Random.Range(0, spones.Length);
+        int rnd_animal = Random.Range(0, animals.Length);
+        Instantiate(animals[rnd_animal]);
+    }
+}
