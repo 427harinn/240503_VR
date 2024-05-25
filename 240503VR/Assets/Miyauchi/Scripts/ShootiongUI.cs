1d2767ccabeabb20d9659c42e45d1ff499a6a383
k21rs125 <98999985+k21rs125@users.noreply.github.com>
2024-05-25 19:26:23 +0900

    UI変更

--- 240503VR/Assets/Miyauchi/Scripts/ShootiongUI.cs
+++ 240503VR/Assets/Miyauchi/Scripts/ShootiongUI.cs
@@ -24,7 +24,7 @@ public class ShootiongUI : MonoBehaviour
         oldHP = ScoreManager.instance.restHP;
 
         restTime = playtime;
-        timeText.text = (int)restTime + "•b";
+        timeText.text = (int)restTime + "";
     }
 
     // Update is called once per frame
@@ -42,7 +42,7 @@ public class ShootiongUI : MonoBehaviour
             {
                 restTime -= Time.deltaTime;
             }
-            timeText.text = (int)restTime + "•b";
+            timeText.text = (int)restTime + "";
 
             if (ScoreManager.instance.score_shootiong != oldScore)
             {
